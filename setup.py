0f8cea015255175b34e577b48c08e702e4b9df31
DrWateryCat <drwaterycat@gmail.com>
2017-05-14 02:44:34 -0400

    MAJOR: fixed __init__ to hopefully include all files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pypurepursuit", 
-      version='0.0.1', 
+      version='0.0.2', 
       description="Pure Pursuit controller based on FRC Team 254's code", 
       url="https://github.com/DrWateryCat/pypurepursuit", 
       author="Kenny Raisbeck",
--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,14 @@
-from adaptivepurepursuitcontroller import AdaptivePurePursuitController
-from circle import Circle
-from interpolable import Interpolable
-from path import Path
-from pathsegment import PathSegment
-from rigidtransform2d import RigidTransform2D
-from rotation2D import Rotation2D
-from translation2D import Translation2D
-from waypoint import Waypoint
+__all__ = ["adaptivepurepursuitcontroller",
+           "circle",
+           "interpolable",
+           "interpolatingdict",
+           "inverseinterpolable",
+           "path",
+           "pathloader",
+           "pathreader",
+           "pathsegment",
+           "peekorator",
+           "rigidtransform2d",
+           "rotation2D",
+           "translation2D",
+           "waypoint"]
