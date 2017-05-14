272064ef48f1da1921ddbfb44a9c3f87008262c4
DrWateryCat <drwaterycat@gmail.com>
2017-05-14 16:10:49 -0400

    Renamed source folder

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name="pypurepursuit", 
-      version='0.0.2', 
+      version='0.0.3', 
       description="Pure Pursuit controller based on FRC Team 254's code", 
       url="https://github.com/DrWateryCat/pypurepursuit", 
       author="Kenny Raisbeck",
-      packages=find_packages())
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3.5"
+          ])
